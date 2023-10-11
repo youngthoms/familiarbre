6379c5fae4cad10f6645177c8cd9dca890139f0a
Thomas <youngthoms@protonmail.com>
2023-10-11 15:00:48 +0200

    Project initialization

---
+++ src/main/java/com/example/familiarbre/FamiliarbreApplication.java
@@ -0,0 +1,13 @@
+package com.example.familiarbre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamiliarbreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamiliarbreApplication.class, args);
+    }
+
+}
