0bcbcf68838344369ea32033bd47b1348044b21f
Thomas <youngthoms@protonmail.com>
2023-11-21 17:27:47 +0100

    Add family-members API endpoint

--- familiarbre/src/main/java/com/application/familiarbre/api/FamiliArbreController.java
+++ familiarbre/src/main/java/com/application/familiarbre/api/FamiliArbreController.java
@@ -1,6 +1,7 @@
 package com.application.familiarbre.api;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class FamiliArbreController {
     @GetMapping
-    public IndexResponse index() {
-        return new IndexResponse("Suuuup");
-    }
-}
-
-class IndexResponse {
-    private String message;
-
-    public IndexResponse(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok("ok");
     }
 }
--- familiarbre/src/main/java/com/application/familiarbre/models/services/FamilyMemberService.java
+++ familiarbre/src/main/java/com/application/familiarbre/models/services/FamilyMemberService.java
@@ -1,7 +1,24 @@
 package com.application.familiarbre.models.services;
 
+import com.application.familiarbre.models.dao.FamilyMemberRepository;
+import com.application.familiarbre.models.entites.FamilyMember;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FamilyMemberService {
+    private FamilyMemberRepository repository;
+
+    public FamilyMember saveFamilyMember(FamilyMember familyMember) {
+        return repository.save(familyMember);
+    }
+
+    public List<FamilyMember> getAll() {
+        return repository.findAll();
+    }
+
+    public FamilyMember getById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
 }
