9c5821534ac5a7dfaefad1ea5e6c060b571a0b66
Thomas <youngthoms@protonmail.com>
2024-01-02 00:19:23 +0100

    Saves the token of the user in DB

--- familiarbre/src/main/java/com/application/familiarbre/authentication/AuthenticationResponse.java
+++ familiarbre/src/main/java/com/application/familiarbre/authentication/AuthenticationResponse.java
@@ -10,5 +10,5 @@ import lombok.*;
 public class AuthenticationResponse {
 
     private String token;
-
+    private String refreshToken;
 }
--- familiarbre/src/main/java/com/application/familiarbre/authentication/AuthenticationService.java
+++ familiarbre/src/main/java/com/application/familiarbre/authentication/AuthenticationService.java
@@ -2,25 +2,32 @@ package com.application.familiarbre.authentication;
 
 import com.application.familiarbre.config.JwtService;
 import com.application.familiarbre.models.dao.FamilyMemberRepository;
+import com.application.familiarbre.models.dao.TokenRepository;
 import com.application.familiarbre.models.dao.UserRepository;
-import com.application.familiarbre.models.entites.FamilyMember;
-import com.application.familiarbre.models.entites.Role;
-import com.application.familiarbre.models.entites.User;
+import com.application.familiarbre.models.entites.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final FamilyMemberRepository familyMemberRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
+
     public AuthenticationResponse register(RegisterRequest request) {
         var user = User.builder()
                 .email(request.getEmail())
@@ -39,6 +46,7 @@ public class AuthenticationService {
         familyMemberRepository.save(familyMember);
 
         var jwtToken = jwtService.generateToken(user);
+        saveUserToken(user, jwtToken);
 
         return AuthenticationResponse.builder().token(jwtToken).build();
     }
@@ -54,7 +62,60 @@ public class AuthenticationService {
         // TODO : refine the exception
         var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
         var jwtToken = jwtService.generateToken(user);
+        // var refreshToken = jwtService.generateRefreshToken(user);
+
+        saveUserToken(user, jwtToken);
 
         return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(Math.toIntExact(user.getId()));
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+        refreshToken = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(refreshToken);
+        if (userEmail != null) {
+            var user = this.userRepository.findByEmail(userEmail)
+                    .orElseThrow();
+            if (jwtService.isTokenValid(refreshToken, user)) {
+                var accessToken = jwtService.generateToken(user);
+                revokeAllUserTokens(user);
+                saveUserToken(user, accessToken);
+                var authResponse = AuthenticationResponse.builder()
+                        .token(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+                new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+            }
+        }
     }
 }
