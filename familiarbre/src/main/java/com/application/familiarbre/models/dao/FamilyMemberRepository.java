52e54783333d45ca3b345c7c584e98126d509ede
Tom <lamberttom@cy-tech.fr>
2024-01-02 22:12:45 +0100

    add accessToUser

--- familiarbre/src/main/java/com/application/familiarbre/models/dao/FamilyMemberRepository.java
+++ familiarbre/src/main/java/com/application/familiarbre/models/dao/FamilyMemberRepository.java
@@ -1,7 +1,11 @@
 package com.application.familiarbre.models.dao;
 
 import com.application.familiarbre.models.entites.FamilyMember;
+import com.application.familiarbre.models.entites.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FamilyMemberRepository  extends JpaRepository<FamilyMember, Long> {
+    Optional<FamilyMember> findByUser(User user);
 }
