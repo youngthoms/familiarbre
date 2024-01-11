3294e98b97a7fd0de6db68db5c2606e61e36a1e0
Thomas <youngthoms@protonmail.com>
2024-01-11 23:37:45 +0100

    Met les arbres publiques dans search tree

--- react/frontend/src/components/Home.component.tsx
+++ react/frontend/src/components/Home.component.tsx
@@ -1,14 +1,11 @@
 import './Home.css';
 import NavigationLinks from "./NavigationLinks.component";
-import FamilyMembersTable from "../features/TreeList/components/FamilyMembersTable";
 
 const Home = () => {
     return (
         <div className="home-container">
             <img src="src/assets/logo.png" alt="logo" className="logo-form-image"/>
             <NavigationLinks/>
-
-            <FamilyMembersTable/>
         </div>
     );
 };
